0cfdc947325c16cda0304ad357b7592531a5a68b
monolut <nicolaiashirov@gmail.com>
2026-03-27 16:04:12 +0200

    Add base securityConfig

--- services/account-service/src/main/java/org/bank/accountservice/AccountServiceApplication.java
+++ services/account-service/src/main/java/org/bank/accountservice/AccountServiceApplication.java
@@ -2,8 +2,13 @@ package org.bank.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "org.bank.accountservice",
+        "org.bank.authcommon"
+})
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
